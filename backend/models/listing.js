ee6c09a566f257f94ffdd34a3f660bef9cc31126
Manas jha <manasjha2233@gmail.com>
2026-03-10 15:42:45 +0530

    Add Listing model schema

---
+++ backend/models/listing.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const listingSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+module.exports = mongoose.model("Listing", listingSchema);
